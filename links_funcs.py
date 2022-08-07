d6aea8abd950b88a88fcc9ad767d9ce92c9719e6
TimurV2 <sabzerov303@gmail.com>
2022-08-08 00:48:56 +0300

    Чистка кода

--- links_funcs.py
+++ links_funcs.py
@@ -1,10 +1,8 @@
-import re
 import csv
 from urlextract import URLExtract
 
 def find_url(message):
-    # regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    # url = re.findall(regex, string)
+
     try:
         string = message.text
         extractor = URLExtract()
