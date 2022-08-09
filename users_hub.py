26511be2bac1109b9aa2a2f9eb9f58c55cba942f
TimurV2 <sabzerov303@gmail.com>
2022-08-09 20:21:58 +0300

    Добавил исключающее значение в notify

--- users_hub.py
+++ users_hub.py
@@ -6,6 +6,7 @@ TELEGRAM_TOKEN = str(os.environ['bot_token'])
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
+
 def add_user(user_id, first_name, last_name, user_name):
     attend = False
     user_info = [user_id, first_name, last_name, user_name]
@@ -22,13 +23,14 @@ def add_user(user_id, first_name, last_name, user_name):
             writer.writerow(user_info)
             writable_file.close()
 
+
 def sent_notify(message):
     msg = message.text
     avoid_id = message.chat.id
     with open('users_hub.csv', 'r') as readable_file:
         reader = csv.DictReader(readable_file)
         for row in reader:
-            if (str(avoid_id) not in row['ID']) and (message.text not in ["Ссылки", "Контакты", "Инициализация", "Оставить отзыв", "Помощь", "Уведомить всех"]):
+            if (str(avoid_id) not in row['ID']) and (message.text not in ["Ссылки", "Контакты", "Инициализация", "Оставить отзыв", "Помощь", "Уведомить всех", "Гороскоп"]):
                 curr_id = int(row['ID'])
                 bot.send_message(curr_id, f'<u>Общее уведомление от пользователя - '
                                           f'@{message.from_user.username}\n</u>' + msg, parse_mode='html')
